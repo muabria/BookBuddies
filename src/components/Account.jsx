62538da0198abc48f11928d7e93d0cd6e70789f8
Marisa <marisavandellos@gmail.com>
2023-10-06 16:11:57 -0700

    started code for account page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,1 +1,12 @@
 /* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
+export default function Account() {
+    return (
+        <div>
+            <h1>{`Hello, ${username}`}</h1>
+            <section>
+                <h2>Checked out:</h2>
+                {}
+            </section>
+        </div>
+    )
+}
