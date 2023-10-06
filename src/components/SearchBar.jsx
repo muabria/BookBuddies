0993c66c6f4873d6bcd1fb00a6cb804c3d0fb6c0
Kat Christensen <katchristensen336@gmail.com>
2023-10-05 20:12:57 -0400

    Update Search Bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
-import Button from '@mui/material/Button'
+import { TextField, Button, Typography } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react"
 
-const SearchBar = () =>{
-    return(
+const SearchBar = ({onSubmit}) => {
+    const [searchText, setSearchText] = useState("");
+    return (
         <>
-        <Button>
-            Button
-        </Button>
-        
+{/*-----------------------------------------TEXT FIELD--------------------------------------- */}
+            <TextField
+                type='text'
+                label="Search Book"
+                size='small'
+                value={searchText}
+                onChange={(event) => setSearchText(event.target.value)}
+                sx={{
+                    backgroundColor: 'white',
+                }}/>
+{/*-----------------------------------------SUBMIT BUTTON--------------------------------------- */}
+            <Button
+                variant="contained"
+                onClick={() => onSubmit(searchText)}
+                sx={{ ml: 2 }}>
+                <SearchIcon />
+            </Button>
         </>
     )
 }
-
 export default SearchBar
